f3c60dac0cac698bc9bc702a273666dd1494d86f
AcerFeng <qinlanfeng@foxmail.com>
2018-05-03 22:57:54 +0800

    补充说明

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,8 +24,16 @@ export default {
     screenshots() {
       html2canvas(document.getElementById('test-target'), {
         useCROS:true,
-        allowTaint: true
+        allowTaint: true,
+        // foreignObjectRendering: true,
       }).then(canvas => {
+        // 特殊情况，如果需要使用 toDataURL() 方法转成 img 标签显示，则必须保证图片能够跨域（需要图片服务器添加Access-Control-Allow-Origin: *），并打开上方的 foreignObjectRendering 配置
+        // let img = new Image();
+        // img.src = canvas.toDataURL();
+        // document.getElementById('result').appendChild(img);
+
+        // 正常情况，即使是跨域图片也能在 canvas 中显示，右键也能保存为图片，可通过设置 style 修改样式，满足需求
+        // 推荐使用以下方式实现
         canvas.style = ``;
         document.getElementById('result').appendChild(canvas);
       });
